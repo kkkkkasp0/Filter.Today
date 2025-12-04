7109f8a94af04217dc46103993d823916fed9bc7
kkkkkasp0 <tmddk5678.54@gmail.com>
2025-12-04 17:58:40 +0900

    Fix: Entity column mappings
    - column 명시

--- src/main/java/com/example/filtertoday/diary/entity/Diary.java
+++ src/main/java/com/example/filtertoday/diary/entity/Diary.java
@@ -19,6 +19,7 @@ import java.time.LocalDateTime;
 public class Diary {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "diary_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
